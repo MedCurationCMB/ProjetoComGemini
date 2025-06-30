4aa3b1e6bc8ffd50deba4ff2e1fc0cfee43a9d7a
sistemapedidocmb <sistemapedido@cmbcapital.com.br>
2025-06-30 16:14:40 -0300

    Adiciona script de atualização de workflow

--- src/pages/visualizacao-indicadores.js
+++ src/pages/visualizacao-indicadores.js
@@ -397,7 +397,7 @@ export default function VisualizacaoIndicadores({ user }) {
               <ResponsiveContainer width="100%" height="100%">
                 <BarChart 
                   data={graficoData} 
-                  margin={{ top: 20, right: 0.5, left: 0.5, bottom: 5 }}
+                  margin={{ top: 20, right: 1, left: 0, bottom: 5 }}
                   barCategoryGap={0} // ✅ FIXO: 5% em vez de variável
                   maxBarSize={calculateBarSize(graficoData.length, isMobile)}
                 >
